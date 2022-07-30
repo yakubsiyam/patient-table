320cc8c410b992403bc52101e422e06a450af0ca
Siyam <itsyakubsiyam@gmail.com>
2022-07-30 13:22:24 +0600

    adding filtering feature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="container-fluid">
       <PatientsTable></PatientsTable>
+      <div className="text-center">
+        <a
+          href="https://admin-dashboard-task.netlify.app/"
+          target="_blank"
+          className="btn btn-primary text-center my-3"
+        >
+          Go To Admin Dashboard
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/PatientsTable.js
+++ src/components/PatientsTable.js
@@ -1,8 +1,9 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { useTable, useGlobalFilter, useFilters, useSortBy } from "react-table";
 import MOCK_DATA from "./MOCK_DATA.json";
 import { COLUMNS } from "./column";
 import ColumnFilter from "./ColumnFilter";
+import { set } from "date-fns";
 
 const FilteringTable = () => {
   const columns = useMemo(() => COLUMNS, []);
@@ -26,20 +27,83 @@ const FilteringTable = () => {
       useSortBy
     );
 
+  let [visibility, setVisibility] = useState({
+    patientVisibility: "invisible",
+    dateVisibility: "invisible",
+    btnVisibility: "invisible",
+    tableVisibility: "invisible",
+  });
+
+  const handleSort = (e) => {
+    const sortValue = e.target.value;
+    if (sortValue != "-1") {
+      setVisibility({
+        patientVisibility: "visible",
+        dateVisibility: "invisible",
+        btnVisibility: "invisible",
+        tableVisibility: "invisible",
+      });
+    } else {
+      setVisibility({
+        patientVisibility: "invisible",
+        dateVisibility: "invisible",
+        btnVisibility: "invisible",
+        tableVisibility: "invisible",
+      });
+    }
+  };
+  const handlePatient = (e) => {
+    const patientValue = e.target.value;
+    if (patientValue != "-1") {
+      setVisibility({
+        patientVisibility: "visible",
+        dateVisibility: "visible",
+        btnVisibility: "invisible",
+        tableVisibility: "invisible",
+      });
+    } else {
+      setVisibility({
+        patientVisibility: "visible",
+        dateVisibility: "invisible",
+        btnVisibility: "invisible",
+        tableVisibility: "invisible",
+      });
+    }
+  };
+  const handleDate = () => {
+    setVisibility({
+      patientVisibility: "visible",
+      dateVisibility: "visible",
+      btnVisibility: "visible",
+      tableVisibility: "invisible",
+    });
+  };
+  const handleSubmit = () => {
+    setVisibility({
+      patientVisibility: "visible",
+      dateVisibility: "visible",
+      btnVisibility: "visible",
+      tableVisibility: "visible",
+    });
+  };
+
   return (
     <div>
-      <section class="py-4">
-        <h5 class="text-gold py-2 fw-bold">Batching Claim(s)</h5>
-        <div class="row">
-          <div class="col-xl-7 col-md-10 row justify-content-around">
-            <div class="col-md-3">
-              <div class="input-group">
-                <p class="fw-bolder w-100 my-2">Sort By</p>
-                <select class="form-select" id="columnName">
+      <section className="py-4">
+        <h5 className="text-gold py-2 fw-bold">Batching Claim(s)</h5>
+        <div className="row">
+          <div className="col-xl-7 col-md-10 row justify-content-around">
+            <div className="col-md-3">
+              <div className="input-group">
+                <p className="fw-bolder w-100 my-2">Sort By</p>
+                <select
+                  className="form-select"
+                  id="columnName"
+                  onChange={handleSort}
+                >
+                  <option value="-1">Select...</option>
                   <option value="1">Id</option>
-                  <option value="2" selected>
-                    Patients
-                  </option>
+                  <option value="2">Patients</option>
                   <option value="3">Treating Therapist</option>
                   <option value="4">Amount</option>
                   <option value="5">Date</option>
@@ -47,13 +111,16 @@ const FilteringTable = () => {
                 </select>
               </div>
             </div>
-            <div class="col-md-5">
-              <div class="input-group">
-                <p class="fw-bolder w-100 my-2">Patient(s)</p>
-                <select class="form-select" id="patientName">
-                  <option value="1" selected>
-                    Domeniga Minmagh
-                  </option>
+            <div className={`col-md-5 ${visibility.patientVisibility}`}>
+              <div className="input-group">
+                <p className="fw-bolder w-100 my-2">Patient(s)</p>
+                <select
+                  className="form-select"
+                  id="patientName"
+                  onChange={handlePatient}
+                >
+                  <option value="-1">Select...</option>
+                  <option value="1">Domeniga Minmagh</option>
                   <option value="2">Jasmine Rawsen</option>
                   <option value="3">Elysia Briars</option>
                   <option value="4">Tani Kimmins</option>
@@ -66,61 +133,77 @@ const FilteringTable = () => {
                 </select>
               </div>
             </div>
-            <div class="col-md-4">
-              <div class="input-group">
-                <p class="fw-bolder w-100 my-2">To Date</p>
-                <input class="form-select" type="date" required />
+            <div className={`col-md-4 ${visibility.dateVisibility}`}>
+              <div className="input-group">
+                <p className="fw-bolder w-100 my-2">To Date</p>
+                <input
+                  className="form-select"
+                  type="date"
+                  required
+                  onChange={handleDate}
+                />
               </div>
             </div>
           </div>
-          <div class="col-xl-5">
-            <p class="my-2 w-100 invisible">space</p>
-            <button class="btn btn-primary me-1">Go</button>
-            <button class="btn btn-danger me-1">Cancel</button>
-            <button class="btn btn-dark me-1" disabled>
+          <div className={`col-xl-5 ${visibility.btnVisibility}`}>
+            <p className="my-2 w-100 invisible">space</p>
+            <button className="btn btn-primary me-1" onClick={handleSubmit}>
+              Go
+            </button>
+            <button className="btn btn-danger me-1">Cancel</button>
+            <button className="btn btn-dark me-1" disabled>
               Generate Batch
             </button>
-            <button class="btn btn-primary">Generate CSV</button>
+            <button className="btn btn-primary">Generate CSV</button>
           </div>
         </div>
-      </section>
-      <h5 class="text-gold py-2 fw-bold">Private Claims</h5>
-      <table {...getTableProps()} class="table table-bordered">
-        <thead className="text-center text-light">
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
-                  scope="col"
-                  style={{ backgroundColor: "#089BAB" }}
-                >
-                  {column.render("Header")}
-                  <span>
-                    {column.isSorted ? (column.isSortedDesc ? "🔽" : "🔼") : ""}
-                  </span>
-                  <div>{column.canFilter ? column.render("Filter") : null}</div>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
 
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+        <div className={`${visibility.tableVisibility}`}>
+          <h5 className="text-gold py-2 fw-bold">Private Claims</h5>
+          <table {...getTableProps()} className="table table-bordered">
+            <thead className="text-center text-light">
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <th
+                      {...column.getHeaderProps(column.getSortByToggleProps())}
+                      scope="col"
+                      style={{ backgroundColor: "#089BAB" }}
+                    >
+                      {column.render("Header")}
+                      <span>
+                        {column.isSorted
+                          ? column.isSortedDesc
+                            ? "🔽"
+                            : "🔼"
+                          : ""}
+                      </span>
+                      <div>
+                        {column.canFilter ? column.render("Filter") : null}
+                      </div>
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell) => {
+                      return (
+                        <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </section>
     </div>
   );
 };
